aa5c86394b7190cd22cecd9604898744559fbba9
Swapnil Tripathi <65762022+swapniltri@users.noreply.github.com>
2023-08-14 15:49:15 +0530

    Website completed

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import "../../styles/ExpenseItem.css";
+import Card from "../UI/Card";
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+const ExpenseItem = (props) => {
+    return (
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date} />
+                <div className="ex pense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>
+            </Card>
+        </li>
+    );
+};
+
+export default ExpenseItem;
